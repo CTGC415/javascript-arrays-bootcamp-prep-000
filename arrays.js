7487f512316758c80040aac5b4a2e396417c2b53
Cully Curwen <c.t.g.curwen@gmail.com>
2019-02-18 19:39:31 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,17 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  return newArray = [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array = [element, ...array]
+}
+
+function addElementToEndOfArray(array, element) {
+  return newArray = [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array = [...array, element]
+}
