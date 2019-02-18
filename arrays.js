5e3805ecb21b776871b3ffc69ecd82cf9f6a9c0b
Cully Curwen <c.t.g.curwen@gmail.com>
2019-02-18 19:51:13 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array = [element, ...array]
+  return array = [element, ...array]
 }
 
 function addElementToEndOfArray(array, element) {
